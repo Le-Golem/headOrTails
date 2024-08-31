f0abe84dd0a5344410d1bbaaab7570fd5dbceaf6
Alexis Baptist <alexisbaptist@Air-de-Alexis.lan>
2024-08-31 06:05:23 +0200

    modification du style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import { getRandomNumber } from './utils';
+import { getRandomNumber } from './utils';  // Assurez-vous que cette fonction est correctement définie et importée
 
-const playerList: { name: string; id: number }[] = [
+const playerList = [
   { name: 'Alexis', id: 0 },
   { name: 'Cédric', id: 1 },
   { name: 'Malcolm', id: 2 },
@@ -12,12 +12,26 @@ const playerList: { name: string; id: number }[] = [
 ];
 
 export default function Home() {
-  const [firstPage , setFirstPage] = useState(false)
-  const [startPlay , setStartPlay] = useState<boolean>(false)
+  const [startPlay, setStartPlay] = useState<boolean>(false);
   const [firstPerson, setFirstPerson] = useState<{ name: string; id: number } | undefined>(undefined);
   const [secondPerson, setSecondPerson] = useState<{ name: string; id: number } | undefined>(undefined);
-  const [headOrTailsResult , setHeadOrTailsResult] = useState<string>("")
-  const [personChoice , setPersonChoice]= useState<string>("")
+  const [headOrTailsResult, setHeadOrTailsResult] = useState<string>("");
+  const [personChoice, setPersonChoice] = useState<string>("");
+
+  const handleFirstPersonChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedId = parseInt(event.target.value, 10);
+    const selectedPerson = playerList.find((player) => player.id === selectedId);
+    setFirstPerson(selectedPerson);
+    if (selectedPerson?.id === secondPerson?.id) {
+      setSecondPerson(undefined);  // Réinitialiser le second choix s'il est égal au premier
+    }
+  };
+
+  const handleSecondPersonChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedId = parseInt(event.target.value, 10);
+    const selectedPerson = playerList.find((player) => player.id === selectedId);
+    setSecondPerson(selectedPerson);
+  };
 
   const startPlayFunction = () => {
     const randomId = getRandomNumber(playerList.length);
@@ -29,90 +43,98 @@ export default function Home() {
     const secondPerson = playerList.find((player) => player.id === randomId2);
     setFirstPerson(firstPerson);
     setSecondPerson(secondPerson);
-    setStartPlay(true)
   };
 
-  const checkWin = (headOrTailsNumber : number) => {
+  const checkWin = (headOrTailsNumber: number) => {
     const randomId = getRandomNumber(2);
-    if (headOrTailsNumber != randomId) {
-      setHeadOrTailsResult("perdu")
+    if (headOrTailsNumber !== randomId) {
+      setHeadOrTailsResult("perdu");
     } else {
-      setHeadOrTailsResult("gagné")
+      setHeadOrTailsResult("gagné");
     }
-  } 
+  };
 
-  useEffect(() => { 
-    setHeadOrTailsResult("")
+  useEffect(() => {
+    setHeadOrTailsResult("");
     if (firstPerson && secondPerson) {
       const randomNumber = getRandomNumber(2);
       setPersonChoice(randomNumber === 0 ? firstPerson.name : secondPerson.name);
     }
   }, [firstPerson, secondPerson]);
-  
 
   return (
-    <article>
-    {firstPage ?
-      <>
-      <div className='flex justify-center items-center h-screen' >
-        <button onClick={() => setFirstPage(false)} className='bg-slate-700'>Start</button>
-      </div>
-      </>
-    : <>
-      <div className='h-screen'>
-      <section className='flex gap-8 justify-center h-12 w-screen mt-24'>
-        <div className='bg-slate-50 h-8 w-32 flex justify-center'>
-          <p style={{color : "black"}}>
-            {firstPerson ? firstPerson.name : ''}
-          </p>
+    <article className='bg-sky-900 w-screen h-screen'>
+      <section className='w-screen h-32 pt-4 pb-4 flex items-center justify-center'>
+        <h1 className="text-7xl" style={{ fontFamily: 'Montserrat, sans-serif' }}>Pile ou face</h1>
+      </section>
+
+      <section className='pl-4 w-screen h-32 flex gap-8 justify-center items-center'>
+        <div className='flex flex-col'>
+          <label htmlFor="firstPersonSelect">1ère personne :</label>
+          <select className='w-36 h-14 text-black' id="firstPersonSelect" value={firstPerson?.id ?? ''} onChange={handleFirstPersonChange}>
+            <option value="" disabled>Sélectionnez une personne</option>
+            {playerList.map((player) => (
+              <option key={player.id} value={player.id}>
+                {player.name}
+              </option>
+            ))}
+          </select>
         </div>
-        <p>X</p> 
-        <div className='bg-slate-50 h-8 w-32 flex justify-center'>
-        <p style={{color : "black"}}>
-            {secondPerson ? secondPerson.name : ''}
-          </p>
+
+        <div className='flex flex-col'>
+          <label htmlFor="secondPersonSelect">2ème personne :</label>
+          <select
+            className='w-36 h-14 text-black'
+            id="secondPersonSelect"
+            value={secondPerson?.id ?? ''}
+            onChange={handleSecondPersonChange}
+            disabled={!firstPerson}
+          >
+            <option value="" disabled>Sélectionnez une personne</option>
+            {playerList
+              .filter((player) => player.id !== firstPerson?.id) // Exclure la première personne sélectionnée
+              .map((player) => (
+                <option key={player.id} value={player.id}>
+                  {player.name}
+                </option>
+              ))}
+          </select>
+        </div>
+        <div className='border-2 p-4 rounded-xl mt-6'>
+         <button onClick={startPlayFunction}>Random</button>
         </div>
       </section>
-        <div className='w-screen flex justify-center'>
-          <button onClick={startPlayFunction}>Random</button>
-        </div>
+
+      <section className='flex w-screen justify-center items-center'>
         {firstPerson &&
-        secondPerson &&
-        firstPerson.name != "" &&
-        secondPerson.name != "" && 
-          <section className='bg-slate-50 w-screen h-32  mt-8'>
-            <div className='flex justify-center items-center'>
-            <p style={{color : "black"}}>{personChoice} choisis pile ou face</p>
+              secondPerson &&
+              firstPerson.name != "" &&
+              secondPerson.name != "" && 
+          <article className='flex w-11/12 justify-center rounded-xl border-2 flex-col items-center mt-8'>
+            <p className='mt-4'>{personChoice} choisis Pile ou face</p>
+
+            <div className='flex gap-8 mt-8 mb-8' >
+              <button className='border-2 p-4 rounded-xl w-24 hover:bg-zinc-100 hover:text-black transition ease-in-out delay-50' onClick={() => checkWin(0)}>pile</button>
+              <button className='border-2 p-4 rounded-xl w-24 hover:bg-zinc-100 hover:text-black transition ease-in-out delay-50' onClick={() => checkWin(1)}>face</button>
             </div>
-            <div className='flex gap-8 w-screen justify-center mt-8' >
-              <button onClick={() => checkWin(0)} style={{color : "black"}}>pile</button>
-              <button onClick={() => checkWin(1)} style={{color : "black"}}>face</button>
-            </div>
-            {headOrTailsResult != "" && <p style={{color : "black"}} className='flex justify-center items-center' >{headOrTailsResult}</p>} 
-          </section>
+
+            {headOrTailsResult != "" && <p className='flex justify-center items-center pb-8' >{headOrTailsResult}</p>} 
+          </article>
         }
-      </div>
-    </> }
+      </section>
 
-      
-      
-      
-      
-      
-      
-      {/* <p>ceci est un test</p>
-      <p>{firstPerson ? firstPerson.name : 'Aucun joueur sélectionné'}</p>
-      <p>{secondPerson ? secondPerson.name : 'Aucun joueur sélectionné'}</p>
-
-    <div className='bg-slate-700 flex flex-col'>
-      <button onClick={startPlayFunction}>Play</button>
-      {startPlay && 
-      <>
-        <button onClick={headOrTailsFunction}>Pile ou face</button> 
-        <p>{headOrTails != "" && headOrTails}</p> 
-      </>
-      }
-    </div>  */}
+            {/* 
+              <section className='bg-sky-900 w-11/12 h-32  mt-8'>
+                <div className='flex justify-center items-center'>
+                <p style={{color : "black"}}>{personChoice} choisis pile ou face</p>
+                </div>
+                <div className='flex gap-8 w-screen justify-center mt-8' >
+                  <button onClick={() => checkWin(0)} style={{color : "black"}}>pile</button>
+                  <button onClick={() => checkWin(1)} style={{color : "black"}}>face</button>
+                </div>
+                {headOrTailsResult != "" && <p style={{color : "black"}} className='flex justify-center items-center' >{headOrTailsResult}</p>} 
+              </section>
+            } */}
     </article>
   );
 }
